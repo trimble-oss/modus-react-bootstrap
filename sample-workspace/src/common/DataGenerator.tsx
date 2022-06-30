b2da233a8ac3e9e9f25433a10cb64e7223cd8ddd
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-06-30 12:30:37 +0530

    Last fix wasn't successful

--- sample-workspace/src/common/DataGenerator.tsx
+++ sample-workspace/src/common/DataGenerator.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 export type DataType = {
   firstName: string;
   lastName: string;
@@ -19,38 +17,38 @@ const range = (len: number) => {
 };
 
 const names = [
-  "Mickey Mouse",
-  "Bugs Bunny",
-  "Homer Simpson",
-  "Fred Flintstone",
-  "Sponge Bob",
-  "Daffy Duck",
-  "Charlie Brown",
-  "Scooby Doo",
-  "Tom Cat",
-  "Jerry Mouse",
-  "Mighty Mouse",
-  "Wile E Coyote",
-  "Tweety Bird",
-  "Pink Panther",
-  "Road Runner",
-  "Patrick Star",
-  "Roger Rabbit",
-  "Papa Smurf",
-  "Buzz Lightyear",
+  'Mickey Mouse',
+  'Bugs Bunny',
+  'Homer Simpson',
+  'Fred Flintstone',
+  'Sponge Bob',
+  'Daffy Duck',
+  'Charlie Brown',
+  'Scooby Doo',
+  'Tom Cat',
+  'Jerry Mouse',
+  'Mighty Mouse',
+  'Wile E Coyote',
+  'Tweety Bird',
+  'Pink Panther',
+  'Road Runner',
+  'Patrick Star',
+  'Roger Rabbit',
+  'Papa Smurf',
+  'Buzz Lightyear',
 ];
 const newPerson = (): DataType => {
   const rand = Math.random();
   const namesIndex = Math.floor(rand * (names.length - 1));
-  const firstName = names[namesIndex].split(" ")[0];
-  const lastName = names[namesIndex].split(" ")[1];
+  const firstName = names[namesIndex].split(' ')[0];
+  const lastName = names[namesIndex].split(' ')[1];
   return {
     firstName,
     lastName,
     age: Math.floor(rand * 30),
     visits: Math.floor(rand * 100),
     progress: Math.floor(rand * 100),
-    status: rand > 0.66 ? "Verified" : rand > 0.33 ? "Pending" : "Rejected",
+    status: rand > 0.66 ? 'Verified' : rand > 0.33 ? 'Pending' : 'Rejected',
   };
 };
 export function MakeData(...lens: any): DataType[] {
